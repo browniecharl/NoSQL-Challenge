65aa7008e0a59432bed363b6f40651a133673728
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-07-09 11:08:08 -0400

    moved reactions from its own model, into the thoughts model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,27 +1,27 @@
-const { Schema, Types } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+// const { Schema, Types } = require('mongoose');
+// const dateFormat = require('../utils/dateFormat');
 
-const ReactionSchema = new Schema (
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            validate: [({ length }) => length <= 280, 'Your reaction may not be more than 280 characters.']
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAt => dateFormat(createdAtVal)
-        }
-    }
-)
+// const ReactionSchema = new Schema (
+//     {
+//         reactionId: {
+//             type: Schema.Types.ObjectId,
+//             default: () => new Types.ObjectId()
+//         },
+//         reactionBody: {
+//             type: String,
+//             required: true,
+//             validate: [({ length }) => length <= 280, 'Your reaction may not be more than 280 characters.']
+//         },
+//         username: {
+//             type: String,
+//             required: true
+//         },
+//         createdAt: {
+//             type: Date,
+//             default: Date.now,
+//             get: createdAt => dateFormat(createdAtVal)
+//         }
+//     }
+// )
 
-module.exports = ReactionSchema;
+// module.exports = ReactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,5 @@
 const { Schema, Types, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
-const reactionSchema = require('./Reaction');
 
 const ThoughtsSchema = new Schema (
     {
@@ -18,7 +17,7 @@ const ThoughtsSchema = new Schema (
             type: String,
             required: true
         },
-        reactions: [reactionSchema],
+        reactions: [ReactionSchema],
     },
     {
         toJSON: {
@@ -31,6 +30,29 @@ const ThoughtsSchema = new Schema (
 ThoughtsSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
+
+const ReactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            validate: [({ length }) => length <= 280, 'Your reaction may not be more than 280 characters.']
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+);
 
 const Thought = model('Thought', ThoughtsSchema);
 
